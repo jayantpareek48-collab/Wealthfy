cdc7657059eb93d9c6d23761b270debb2428d585
JAYANT PAREEK <jayantpareek48@gmail.com>
2026-04-25 16:22:27 +0530

    Delete stocks.js

--- stocks.js
+++
@@ -1,6 +0,0 @@
-export const initialStocks = [
-  { symbol: "RELIANCE", name: "Reliance Industries", price: 2950 },
-  { symbol: "TCS", name: "Tata Consultancy", price: 3920 },
-  { symbol: "HDFCBANK", name: "HDFC Bank", price: 1655 },
-  { symbol: "INFY", name: "Infosys", price: 1460 }
-];
